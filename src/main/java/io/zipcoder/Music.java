7b9b9a8acdeac466bdd7627734cb1a63daac6964
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-21 22:23:08 -0500

    lab completed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,34 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        Integer upCount = 0;
+        Integer downCount = 0;
+        Integer upIndex = startIndex;
+        Integer downIndex = startIndex;
+
+        for (int i = 0; i < playList.length; i++) {
+            if (playList[upIndex].equals(selection) || playList[downIndex].equals(selection)) {
+                break;
+            }
+
+            if (upIndex == 0) {
+                upIndex = playList.length-1;
+            } else {
+                upIndex--;
+            }
+            upCount++;
+
+            if (downIndex == playList.length-1) {
+                downIndex = 0;
+            } else {
+                downIndex++;
+            }
+            downCount++;
+        }
+
+        if (downCount < upCount) {
+            return downCount;
+        }
+        return upCount;
     }
 }
